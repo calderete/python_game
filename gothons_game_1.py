340a07edf696cc387e79ad68db95338676488460
Charles <codprestige10@gmail.com>
2016-04-21 13:14:32 -0400

    added a python web crawler

--- gothons_game_1.py
+++ gothons_game_1.py
@@ -68,21 +68,96 @@ class CentralCorridor(scene):
 			print "your head and eats you"
 			return 'death'
 
+		elif action = "tell a joke":
+			print "Lucky for you they made you learn Gothon insults in the academy."
+			print "You tell the one Gothon joke you know:"
+			print "Lbhe zbgure vf fb sng, jura fur fvgt nebhaz fur ubnfr, fur fvgf nebhaz gur ubhfr."
+			print "while he's laughing you run up and shoot him square in the head"
+			print "putting him down, then jump through the Weapon Armory door."
+
+		else:
+			print "DOES NOT COMPUTE"
+			return 'central_corridor'
+
 
 class LaserWeaponArmory(Scene):
 
 	def enter(self):
-		pass
+		print "Your do a dive roll into the Weapon Armory, crouch and scan the room"
+		print "for more Gothons that might be hiding.  It's dead quiet, too quiet."
+		print "You stand up and run to the far side of the room and find the"
+		print "neurtron bomb in its container.  There's a keypad lock on the box"
+		print "and you need the code to get the bomb out.  If you get the code"
+		print "wrong 10 times then the lock closes forever and you can't"
+		print "get the bomb.  The code is 3 digits."
+		code = "%d%d%d" % (randint(1, 9), randint(1, 9), randint(1, 9))
+		guess = raw_input("[keypad]> ")
+		guesses = 0 
+
+		while guess != code and guesses < 10:
+			print "BZZZZZZZZEDDDD!"
+			guesses += 1
+			guess = raw_input("[keypad]> ")
+		if guess == code:
+			print "The container clicks open and the seal breaks, letting gas out."
+			print "You grab the neutron bomb and run as fast as you can to the"
+			print " bridge where you must place it in the right spot."
+			return 'the_bridge'
+		else:
+			print "The lock bzzes one last time and then you hear a sickening"
+			print "melting sound as the mechanism is fused together."
+			print "You decide to sit there, and finally the Gothons blow up the"
+			print "ship from their ship and you die."
+			return 'death'
 
 class TheBridge(Scene):
 
 	def enter(self):
-		pass
+		print "Your burst onto the Bridge with the neutron destruct bomb"
+		print "under your arm and surprise 5 Gothons who are trying to"
+		print "take control of the ship.  Each of them has an even uglier"
+		print "clown costume than the last.  They haven't pulled their"
+		print "weapons out yet, as they see the active bombunder your"
+		print "arm and dont't want to set it off."
+
+		action = raw_input("> ")
+
+		if action == "throw the bomb":
+			print "In a panic you throw the bomb at the group of Gothons"
+			print "and make a leap for the door.  Right as you drop it a"
+			print "Gothon shoots you right in the back killing you."
+			print "As you die you see another Gothon frantically try to disarm"
+			print "the bomb.  You die knowing they will probably blow up when"
+			print "it goes off"
+			return 'death'
+
+		elif action == "slowly place the bomb":
+			print "You point your blaster at the bomb under your arm"
+			print "and the Gothons put their hands up and start to sweat."
+			print "You inch backward to the door, open it, and then carefully"
+			print "place the bomb on the floor, pointing your blaster at it"
+			print "and blast the lock so the Gothons can't get out"
+			print "Now that the bomb is placed you run to the escape pod to"
+			print "get off this tin can"
+			return 'escape_pod'
+		else:
+			print "DOES NOT COMPUTE"
+			return 'the_bridge'
+
 
 class EscapePod(Scene):
 
 	def enter(self):
-		pass
+		print "You rush through the ship despreately trying to make it to"
+		print "the escape pod before the whole ship explodes.  It seems like"
+		print "hardly any Gothons are on the ship, so your run is clear of"
+		print "interference.  You get to the chamber with the escape pods, and"
+		print "now need to pick one to take.  Some of them could be damaged"
+		print " byt you don't have time to look.  There's 5 pods, which one"
+		print "do you take?"
+
+		good_pod = randint(1, 5)
+		guess = raw_input("[pod #> ")
 
 class Map(object):
 
